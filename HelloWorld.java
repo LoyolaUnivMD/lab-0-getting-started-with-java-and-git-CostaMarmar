c8478641b21dcad77fd27f4b9c17100814bdb3cb
Costamarmar <Cmarmaras1@gmail.com>
2024-01-19 18:20:46 -0500

    wrote a C

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,11 @@
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("----------");
+        System.out.println("-----------");
         for(int i = 0; i < 5; i++)
         {
             System.out.println("|");
         }
-        System.out.println("----------");
+        System.out.println("-----------");
     }
 }
